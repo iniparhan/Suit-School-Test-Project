83ee6b59f1d395a1cb730adb91a34c37c71e9a92
Ahmad "Farhan" QF <farhanhmd11@gmail.com>
2026-06-07 23:27:31 +0700

    Initial Commit

---
+++ Suit-School-Test-Project/Suit_School_Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Suit_School_Test_ProjectApp.swift
+//  Suit-School-Test-Project
+//
+//  Created by ahmadfarhanqf on 07/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Suit_School_Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
